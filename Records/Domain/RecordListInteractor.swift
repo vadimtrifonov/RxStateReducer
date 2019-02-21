098b74a48bf3bfaa5ce17ad872a5224c04790caf
Vadim Trifonov <va.trifonov@gmail.com>
2019-02-22 08:25:04 +1100

    Move same layer implementation closer to protocol

--- Records/Domain/RecordListInteractor.swift
+++ Records/Domain/RecordListInteractor.swift
@@ -1,6 +1,47 @@
+import Foundation
 import RxSwift
 import RxCocoa
 
 public protocol RecordListInteractor {
     func start(actions: Signal<RecordListAction>) -> Driver<RecordListState>
+}
+
+public final class RecordListInteractorImpl: RecordListInteractor {
+    private let gateway: RecordGateway
+    
+    init(gateway: RecordGateway) {
+        self.gateway = gateway
+    }
+    
+    public func start(actions: Signal<RecordListAction>) -> Driver<RecordListState> {
+        return actions
+            .startWith(.reload)
+            .flatMap(handleAction)
+            .scan(RecordListState.initial, accumulator: RecordListState.reduce)
+            .asDriver()
+    }
+    
+    private func handleAction(_ action: RecordListAction) -> Signal<RecordListState.Mutation> {
+        switch action {
+        case .toggle:
+            return toggleRecord()
+        case .reload:
+            return fetchRecords()
+        }
+    }
+    
+    private func toggleRecord() -> Signal<RecordListState.Mutation> {
+        return gateway.toggleRecord()
+            .andThen(fetchRecords().asObservable())
+            .startWith(RecordListState.Mutation.loading)
+            .asSignal(onErrorRecover: { .just(RecordListState.Mutation.failure($0)) })
+    }
+    
+    private func fetchRecords() -> Signal<RecordListState.Mutation> {
+        return gateway.fetchRecords()
+            .asObservable()
+            .map(RecordListState.Mutation.records)
+            .startWith(RecordListState.Mutation.loading)
+            .asSignal(onErrorRecover: { .just(RecordListState.Mutation.failure($0)) })
+    }
 }
